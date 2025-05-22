28ae51571161c51b94a5d71c75f30ca261d936de
Maksym <121174066+MaksymIT@users.noreply.github.com>
2025-05-22 20:43:32 +0200

    Опис змін: компонент FriendList відредаговано

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,13 +1,18 @@
 import FriendListItem from "../friendListItem/FriendListItem";
+import styles from './FriendList.module.css'
 
-export default function FriendList({friends}) {
-    return (
-<ul>
-	<li>
-        <FriendListItem friends={friends} />
-	</li>
-</ul>
-
-    )
+export default function FriendList({ friends }) {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(friend => (
+        <li key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
-
--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -1,21 +1,26 @@
 import clsx from 'clsx';
 import styles from './FriendListItem.module.css'
 
-export default function FriendListItem({ friends }) {
+export default function FriendListItem({ avatar, name, isOnline, id }) {
   return (
-     <div className='flex'> 
-      {
-      friends.map(({avatar, name, isOnline, id}) => (
-        <div key={id}><img className='img' src={avatar} alt="Avatar" width="48" />
-          <p className={clsx(isOnline ? styles.true : styles.false)}>
-            {isOnline ? 'Online' : 'Offline'}
-          </p>
-          <p>{name}</p>
-      </div>
-     ))}
-    </div>
-    
+  <div key={id}>
+  <img src={avatar} alt="Avatar" width="48" />
+    <p>{name}</p>
+    <p className={clsx(isOnline ? styles.true : styles.false)}>
+    {isOnline ? 'Online' : 'Offline'}
+    </p>
+  </div>
+
   );
 };
 
 
+    // {
+    //   friends.map(({avatar, name, isOnline, id}) => (
+    //     <div key={id}><img className='img' src={avatar} alt="Avatar" width="48" />
+    //       <p className={clsx(isOnline ? styles.true : styles.false)}>
+    //         {isOnline ? 'Online' : 'Offline'}
+    //       </p>
+    //       <p>{name}</p>
+    //   </div>
+    //  ))}
